2451c62ff7df63c8aaaa5069f2274033d887b800
SohaibRaza66 <sohaibraza236@gmail.com>
2025-12-06 19:35:26 +0500

    again change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api" || import.meta.env.VITE_API_BASE,
+  baseURL: import.meta.env.VITE_API_BASE,
 });
 
 api.interceptors.request.use((config) => {
